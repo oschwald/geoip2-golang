cefd65f61dca898c8baa611fc3d8bd1f0ca525e6
Gregory Oschwald <goschwald@maxmind.com>
2014-03-16 12:52:47 -0700

    Revert Travis changes

--- example_test.go
+++ example_test.go
@@ -20,11 +20,13 @@ func Example() {
 		panic(err)
 	}
 	fmt.Printf("Portuguese (BR) city name: %v\n", record.City.Names["pt-BR"])
-	fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
+	fmt.Printf("English subdivision name: %v\n",
+		record.Subdivisions[0].Names["en"])
 	fmt.Printf("Russian country name: %v\n", record.Country.Names["ru"])
 	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
 	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
-	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude,
+		record.Location.Longitude)
 
 	db.Close()
 	// Output:
