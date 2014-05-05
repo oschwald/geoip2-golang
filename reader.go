aaeb576dc1c0acb142859b4c9761269b3774b9bd
Gregory Oschwald <goschwald@maxmind.com>
2014-05-04 18:28:01 -0700

    Corrected types and added benchmarking code

--- reader.go
+++ reader.go
@@ -13,78 +13,78 @@ import (
 // databases.
 type City struct {
 	City struct {
-		GeoNameID int `maxminddb:"geoname_id"`
-		Names     map[string]string
-	}
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		Names     map[string]string `maxminddb:"names"`
+	} `maxminddb:"city"`
 	Continent struct {
-		Code      string
-		GeoNameID int `maxminddb:"geoname_id"`
-		Names     map[string]string
-	}
+		Code      string            `maxminddb:"code"`
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		Names     map[string]string `maxminddb:"names"`
+	} `maxminddb:"continent"`
 	Country struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
-	}
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
+	} `maxminddb:"country"`
 	Location struct {
-		Latitude  float64
-		Longitude float64
-		MetroCode int    `maxminddb:"metro_code"`
-		TimeZone  string `maxminddb:"time_zone"`
-	}
+		Latitude  float64 `maxminddb:"latitude"`
+		Longitude float64 `maxminddb:"longitude"`
+		MetroCode uint    `maxminddb:"metro_code"`
+		TimeZone  string  `maxminddb:"time_zone"`
+	} `maxminddb:"location"`
 	Postal struct {
-		Code string
-	}
+		Code string `maxminddb:"code"`
+	} `maxminddb:"postal"`
 	RegisteredCountry struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
 	} `maxminddb:"registered_country"`
 	RepresentedCountry struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
-		Type      string
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
+		Type      string            `maxminddb:"type"`
 	} `maxminddb:"represented_country"`
 	Subdivisions []struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
-	}
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
+	} `maxminddb:"subdivisions"`
 	Traits struct {
 		IsAnonymousProxy    bool `maxminddb:"is_anonymous_proxy"`
 		IsSatelliteProvider bool `maxminddb:"is_satellite_provider"`
-	}
+	} `maxminddb:"traits"`
 }
 
 // The Country structure corresponds to the data in the GeoIP2/GeoLite2
 // Country databases.
 type Country struct {
 	Continent struct {
-		Code      string
-		GeoNameID int `maxminddb:"geoname_id"`
-		Names     map[string]string
-	}
+		Code      string            `maxminddb:"code"`
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		Names     map[string]string `maxminddb:"names"`
+	} `maxminddb:"continent"`
 	Country struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
-	}
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
+	} `maxminddb:"country"`
 	RegisteredCountry struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
 	} `maxminddb:"registered_country"`
 	RepresentedCountry struct {
-		GeoNameID int    `maxminddb:"geoname_id"`
-		IsoCode   string `maxminddb:"iso_code"`
-		Names     map[string]string
-		Type      string
+		GeoNameID uint              `maxminddb:"geoname_id"`
+		IsoCode   string            `maxminddb:"iso_code"`
+		Names     map[string]string `maxminddb:"names"`
+		Type      string            `maxminddb:"type"`
 	} `maxminddb:"represented_country"`
 	Traits struct {
 		IsAnonymousProxy    bool `maxminddb:"is_anonymous_proxy"`
 		IsSatelliteProvider bool `maxminddb:"is_satellite_provider"`
-	}
+	} `maxminddb:"traits"`
 }
 
 // Reader holds the maxminddb.Reader structure. It should be created
--- reader_test.go
+++ reader_test.go
@@ -1,9 +1,12 @@
 package geoip2
 
 import (
+	"fmt"
 	. "launchpad.net/gocheck"
+	"math/rand"
 	"net"
 	"testing"
+	"time"
 )
 
 func TestGeoIP2(t *testing.T) { TestingT(t) }
@@ -25,7 +28,7 @@ func (s *MySuite) TestReader(c *C) {
 		c.Fail()
 	}
 
-	c.Assert(record.City.GeoNameID, Equals, 2643743)
+	c.Assert(record.City.GeoNameID, Equals, uint(2643743))
 	c.Assert(record.City.Names, DeepEquals, map[string]string{
 		"de":    "London",
 		"en":    "London",
@@ -35,7 +38,7 @@ func (s *MySuite) TestReader(c *C) {
 		"pt-BR": "Londres",
 		"ru":    "Лондон",
 	})
-	c.Assert(record.Continent.GeoNameID, Equals, 6255148)
+	c.Assert(record.Continent.GeoNameID, Equals, uint(6255148))
 	c.Assert(record.Continent.Code, Equals, "EU")
 	c.Assert(record.Continent.Names, DeepEquals, map[string]string{
 		"de":    "Europa",
@@ -48,7 +51,7 @@ func (s *MySuite) TestReader(c *C) {
 		"zh-CN": "欧洲",
 	})
 
-	c.Assert(record.Country.GeoNameID, Equals, 2635167)
+	c.Assert(record.Country.GeoNameID, Equals, uint(2635167))
 	c.Assert(record.Country.IsoCode, Equals, "GB")
 	c.Assert(record.Country.Names, DeepEquals, map[string]string{
 		"de":    "Vereinigtes Königreich",
@@ -65,7 +68,7 @@ func (s *MySuite) TestReader(c *C) {
 	c.Assert(record.Location.Longitude, Equals, -0.0931)
 	c.Assert(record.Location.TimeZone, Equals, "Europe/London")
 
-	c.Assert(record.Subdivisions[0].GeoNameID, Equals, 6269131)
+	c.Assert(record.Subdivisions[0].GeoNameID, Equals, uint(6269131))
 	c.Assert(record.Subdivisions[0].IsoCode, Equals, "ENG")
 	c.Assert(record.Subdivisions[0].Names, DeepEquals, map[string]string{
 		"en":    "England",
@@ -74,7 +77,7 @@ func (s *MySuite) TestReader(c *C) {
 		"es":    "Inglaterra",
 	})
 
-	c.Assert(record.RegisteredCountry.GeoNameID, Equals, 6252001)
+	c.Assert(record.RegisteredCountry.GeoNameID, Equals, uint(6252001))
 	c.Assert(record.RegisteredCountry.IsoCode, Equals, "US")
 	c.Assert(record.RegisteredCountry.Names, DeepEquals, map[string]string{
 		"de":    "USA",
@@ -103,7 +106,27 @@ func (s *MySuite) TestMetroCode(c *C) {
 		c.Fail()
 	}
 
-	c.Assert(record.Location.MetroCode, Equals, 819)
+	c.Assert(record.Location.MetroCode, Equals, uint(819))
 
 	reader.Close()
+}
+
+func BenchmarkMaxMindDB(b *testing.B) {
+	db, err := Open("GeoLite2-City.mmdb")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < b.N; i++ {
+		num := r.Uint32()
+		ip := net.ParseIP(fmt.Sprintf("%d.%d.%d.%d", (0xFF000000&num)>>24,
+			(0x00FF0000&num)>>16, (0x0000FF00&num)>>8, 0x000000F&num))
+		_, err := db.City(ip)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	db.Close()
 }
